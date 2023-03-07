99dc438c0da7e91efd01c6b2bf076ba82e4cb85e
Maria Suarez <msuarezh@gmail.com>
2023-03-07 20:56:29 +0100

    hamburguer menu done

---
+++ main.js
@@ -0,0 +1,14 @@
+const nav = document.querySelector('#navSideMenu')
+const closeIcon = document.querySelector('#navClose')
+const openIcon = document.querySelector('#navOpen')
+
+const navOpen = () => {
+    nav.className = 'navLinks navLinksVisible'
+}
+
+const navClose = () => {
+    nav.className = 'navLinks'
+}
+
+openIcon.addEventListener('click', navOpen);
+closeIcon.addEventListener('click', navClose);
