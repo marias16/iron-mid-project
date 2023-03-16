aa3ed4a5fe1822b2d469be7bdc3086d7a3184176
Maria Suarez <msuarezh@gmail.com>
2023-03-16 20:40:33 +0100

    mobile menu minor fixes

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-
-
 // menu mobile
 const nav = document.querySelector('#navSideMenu')
 const closeIcon = document.querySelector('#navClose')
@@ -7,12 +5,41 @@ const openIcon = document.querySelector('#navOpen')
 
 const navOpen = () => {
     nav.className = 'navLinks navLinksVisible'
+    disableScroll();
 }
 
 const navClose = () => {
     nav.className = 'navLinks'
+    enableScroll();
 }
 
 openIcon.addEventListener('click', navOpen);
 closeIcon.addEventListener('click', navClose);
 
+//when clicking on a menu link, close the menu
+
+let arrNav = Array.prototype.slice.call(document.querySelectorAll("#navSideMenu > .htextMedium"))
+arrNav.push(document.querySelector('#navCTAMobile'))
+
+clickNavClose = (arr) => {
+    arr.forEach((element) => {
+        element.addEventListener('click', navClose)
+    })
+}
+
+clickNavClose(arrNav)
+
+//when clicking on the menu, disable scrolling
+
+function disableScroll() {
+    scrollTop = window.pageYOffset 
+    scrollLeft = window.pageXOffset 
+  
+    window.onscroll = function() {
+        window.scrollTo(scrollLeft, scrollTop);
+    };
+}
+  
+function enableScroll() {
+    window.onscroll = function() {};
+}
